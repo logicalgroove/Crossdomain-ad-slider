8b13560a672f6829a8396f6f92f8a704f6f694fb
Alexander Lopez <webodessa@gmail.com>
2011-03-09 16:55:02 +0200

    minor changes for tumblr compatibility

--- client/slider-script_old.js
+++ client/slider-script_old.js
@@ -44,7 +44,7 @@ function initJQuery() {
               var dataArr = new Array();
 
               //parsing data from loaded JSON
-              j.each(data.slides.slide, function(i,item){
+              j.each(data.slide, function(i,item){
                 var image_tag = '<a href="'+ item.link +'" target="_blank"><img src="'+ item.image +'" /></a>';
                 var title_tag = '<a class="slider-title" href="'+ item.link +'" target="_blank">'+ item.title +'</a>';
                 var comment_tag = '<span class="slider-comment">'+ item.comment +'</span>';
